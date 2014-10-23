683daa7aa36b36a06916128b2605deca9c26cc7b
Dreamsxin <dreamsxin@126.com>
2014-10-23 11:39:45 +0800

    Init

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Dreamszhu
+ *
+ * Created on October 23, 2014, 11:36 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
